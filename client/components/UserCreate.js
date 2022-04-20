8567b7dcac205e21c0859334c5ebdea163648654
Mason Swager <mlswager2@gmail.com>
2022-04-20 16:41:03 -0500

    some styling for required fields

--- client/components/UserCreate.js
+++ client/components/UserCreate.js
@@ -38,9 +38,9 @@ function UserCreate(){
             if (trigger){
                 dispatch(signup({ ...user }))
             }else{
-                alert("please fill in required fields")
                 setHideRequiredFlag(false)
                 setUser({...user,password:"",confirmPassword:""})
+                alert("please fill in required fields")
             }
         }else{
             alert("passwords do not match")
@@ -50,22 +50,22 @@ function UserCreate(){
     }
     return(
         <form id='todo-form' onSubmit={handleSubmit}>
-            <label htmlFor='firstName'>first name{hideRequiredFlag ? "" : " (required)"}:</label>
+            <label htmlFor='firstName'>first name: {hideRequiredFlag ? "" : <span className = "required"> *required</span>}</label>
             <input name='firstName' value={user.firstName} onChange={(event)=>setUser({...user, firstName:event.target.value})}/>
 
-            <label htmlFor='lastName'>last name{hideRequiredFlag ? "" : " (required)"}:</label>
+            <label htmlFor='lastName'>last name: {hideRequiredFlag ? "" : <span className = "required"> *required</span>}</label>
             <input name='lastName' value={user.lastName} onChange={(event)=>setUser({...user, lastName:event.target.value})}/>
 
-            <label htmlFor='email'>email{hideRequiredFlag ? "" : " (required)"}:</label>
+            <label htmlFor='email'>email: {hideRequiredFlag ? "" : <span className = "required"> *required</span>}</label>
             <input name='email' type="email" value={user.email} onChange={(event)=>setUser({...user, email:event.target.value})}/>
 
-            <label htmlFor='username'>username{hideRequiredFlag ? "" : " (required)"}:</label>
+            <label htmlFor='username'>username: {hideRequiredFlag ? "" : <span className = "required"> *required</span>}</label>
             <input name='username' value={user.username} onChange={(event)=>setUser({...user, username:event.target.value})}/>
 
-            <label htmlFor='password'>password{hideRequiredFlag ? "" : " (required)"}:</label>
+            <label htmlFor='password'>password: {hideRequiredFlag ? "" : <span className = "required"> *required</span>}</label>
             <input name='password' type="password" value={user.password} onChange={(event)=>setUser({...user, password:event.target.value})}/>
 
-            <label htmlFor='confirmPassword'>confirm password{hideRequiredFlag ? "" : " (required)"}:</label>
+            <label htmlFor='confirmPassword'>confirm password: {hideRequiredFlag ? "" : <span className = "required"> *required</span>}</label>
             <input name='confirmPassword' type="password" value={user.confirmPassword} onChange={(event)=>setUser({...user, confirmPassword:event.target.value})}/>
 
             <label htmlFor='img'>image URL:</label>
