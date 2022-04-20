c2cf75f345fb1b26dcadbb48b9bb3095db7007bc
Mason Swager <mlswager2@gmail.com>
2022-04-20 16:12:19 -0500

    build: created userCreate page

--- client/components/UserCreate.js
+++ client/components/UserCreate.js
@@ -14,42 +14,58 @@ function UserCreate(){
         confirmPassword: "",
         img: "",
     })
+    const [hideRequiredFlag,setHideRequiredFlag] = useState(true)
 
     function handleSubmit(event) {
         event.preventDefault();
-        if(user.password===user.confirmpassword){
-            let addUser = {}
+        
+        if(user.password===user.confirmPassword){
+            let trigger = true
+            let addUser ={}
             for (let key in user){
-                console.log(user[key])
-                if(user[key] && key!=="confirmpassword"){
-                    addUser[key] = user[key]
+                if(user[key]){
+                    if(key!=="confirmPassword"){
+                        addUser[key] = user[key]
+                    }
                 }
+                else if(!user[key] && key !== "img"){
+                    console.log(key)
+                    trigger = false
+                }
+                console.log(trigger)
             }
-            dispatch(signup({ ...user }));
+            console.log(addUser)
+            if (trigger){
+                dispatch(signup({ ...user }))
+            }else{
+                alert("please fill in required fields")
+                setHideRequiredFlag(false)
+                setUser({...user,password:"",confirmPassword:""})
+            }
         }else{
+            alert("passwords do not match")
+            setUser({...user,password:"",confirmPassword:""})
             console.log("password mismatch")
         }
     }
-
-    console.log(user)
     return(
         <form id='todo-form' onSubmit={handleSubmit}>
-            <label htmlFor='firstName'>first name:</label>
+            <label htmlFor='firstName'>first name{hideRequiredFlag ? "" : " (required)"}:</label>
             <input name='firstName' value={user.firstName} onChange={(event)=>setUser({...user, firstName:event.target.value})}/>
 
-            <label htmlFor='lastName'>last name:</label>
+            <label htmlFor='lastName'>last name{hideRequiredFlag ? "" : " (required)"}:</label>
             <input name='lastName' value={user.lastName} onChange={(event)=>setUser({...user, lastName:event.target.value})}/>
 
-            <label htmlFor='email'>email:</label>
-            <input name='email' value={user.email} onChange={(event)=>setUser({...user, email:event.target.value})}/>
+            <label htmlFor='email'>email{hideRequiredFlag ? "" : " (required)"}:</label>
+            <input name='email' type="email" value={user.email} onChange={(event)=>setUser({...user, email:event.target.value})}/>
 
-            <label htmlFor='username'>username:</label>
+            <label htmlFor='username'>username{hideRequiredFlag ? "" : " (required)"}:</label>
             <input name='username' value={user.username} onChange={(event)=>setUser({...user, username:event.target.value})}/>
 
-            <label htmlFor='password'>password:</label>
+            <label htmlFor='password'>password{hideRequiredFlag ? "" : " (required)"}:</label>
             <input name='password' type="password" value={user.password} onChange={(event)=>setUser({...user, password:event.target.value})}/>
 
-            <label htmlFor='confirmPassword'>confirm password:</label>
+            <label htmlFor='confirmPassword'>confirm password{hideRequiredFlag ? "" : " (required)"}:</label>
             <input name='confirmPassword' type="password" value={user.confirmPassword} onChange={(event)=>setUser({...user, confirmPassword:event.target.value})}/>
 
             <label htmlFor='img'>image URL:</label>
