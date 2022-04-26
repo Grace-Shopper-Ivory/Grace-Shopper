569f33fb8ffb915222d7e8b50a63850657e472ac
Cody Lester <kcodylester@gmail.com>
2022-04-25 22:15:59 -0400

    continued css formatting

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -8,29 +8,32 @@ import auth from '../store/auth';
 const Navbar = ({ handleClick, isLoggedIn, userInfo }) => (
   <div>
     <header>
-      <h1>The Market of Bread Stapled to Trees</h1>
         <img src="https://i.ibb.co/rkVtJL6/image.png" className="logo" />
-
-        <Link to={`/userpage/${userInfo.id}`}>
-        <img src={userInfo.imageUrl} className="usericon"/>
-        </Link>
+        <h1>The Market for Bread Stapled to Trees</h1>
       {isLoggedIn ? (
         <div>
-          <div>
-            {/* The navbar will show these links after you log in */}
-            <Link to="/">Home</Link>
-            <Link to={`/userpage/${userInfo.id}`}>Userpage</Link>
-            <Link to={`/order`}>Shopping Cart</Link>
-            
-
-            <a href="#" onClick={handleClick}>Logout</a>
-          </div>
+          <Link to={`/userpage/${userInfo.id}`}>
+          <img src={userInfo.imageUrl} className="usericon"/>
+          </Link>
+            <h3>
+              {/* The navbar will show these links after you log in */}
+              <Link to="/"><h3>Home</h3></Link>
+              <Link to={`/userpage/${userInfo.id}`}><h3>Userpage</h3></Link>
+              <Link to={`/order`}><h3>Shopping Cart</h3></Link>
+            </h3>
+            <Link to="/"><p href="#" onClick={handleClick}>Logout</p></Link>
         </div>
       ) : (
         <div>
           {/* The navbar will show these links before you log in */}
+          <h3>          
+          <Link to="/">Home</Link>
+          <Link to={`/order`}>Shopping Cart</Link>
+          </h3>
+          <p>
           <Link to="/login">Login</Link>
           <Link to="/signup">Sign Up</Link>
+          </p>
         </div>
       )}
     </header>
