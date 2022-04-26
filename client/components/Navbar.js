922d8b0e6c67d5d5b1133fc9eab808172f152bc5
Cody Lester <kcodylester@gmail.com>
2022-04-26 17:46:48 -0400

    actually finished welcome bar

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -12,40 +12,41 @@ import auth from '../store/auth';
 // `;
 
 const Navbar = ({ handleClick, isLoggedIn, userInfo }) => (
-    <div>
-      {isLoggedIn ? (
-        <div className='nav'>
-          <div className='top'>
-            {/* The navbar will show these links after you log in */}
-            <img src="https://i.ibb.co/rkVtJL6/image.png" className="logo"/>
-            <h1>The Market for Bread <br/>Stapled to Trees</h1>         
-            <Link to={`/userpage/${userInfo.id}`}>
-              <img src={userInfo.imageUrl} className="usericon"/>
-            </Link>
-          </div>
-          <div className='bar'>
-              <Link to="/"><p>home</p></Link>
-              <Link to={`/userpage/${userInfo.id}`}><p>userpage</p></Link>
-              <Link to={`/order`}><p>shopping cart</p></Link>
-              <Link to="/" href="#" onClick={handleClick}><b>logout</b></Link>
-          </div>
+  <div>
+    {isLoggedIn ? (
+      <div className='nav'>
+        <div className='top'>
+          {/* The navbar will show these links after you log in */}
+          <img src="https://i.ibb.co/rkVtJL6/image.png" className="logo"/>
+          <h1>The Market for Bread <br/>Stapled to Trees</h1>         
+          <Link to={`/userpage/${userInfo.id}`}>
+            <img src={userInfo.imageUrl} className="usericon"/>
+          </Link>
         </div>
-      ) : (
-        <div className='nav'>
+        <div className='bar'>
+            <Link to="/"><p>home</p></Link>
+            <Link to={`/userpage/${userInfo.id}`}><p>userpage</p></Link>
+            <Link to={`/order`}><p>shopping cart</p></Link>
+            <Link to="/" href="#" onClick={handleClick}><b>logout</b></Link>
+        </div>
+      </div>
+    ) : (
+      <div className='nav'>
+        <div className='top'>
           {/* The navbar will show these links before you log in */}
           <img src="https://i.ibb.co/rkVtJL6/image.png" className="logo"/>
-        <h1>The Market for Bread <br/>Stapled to Trees <br/> welcome guest</h1>
-          <ul>          
-          <Link to="/">Home</Link>
-          <Link to={`/order`}>Shopping Cart</Link>
-          </ul>
-          <p>
-          <Link to="/login">Login</Link>
-          <Link to="/signup">Sign Up</Link>
-          </p>
+          <h1>The Market for Bread <br/>Stapled to Trees <br/> welcome guest</h1>
+          <img src="https://i.ibb.co/rkVtJL6/image.png" className="logo"/>
         </div>
-      )}
-    </div>
+        <div className='bar'>
+          <Link to="/"><p>home</p></Link>
+          <Link to={`/order`}><p>shopping cart</p></Link>
+          <Link to="/login"><p>login</p></Link>
+          <Link to="/signup"><p>sign up</p></Link>
+        </div>
+      </div>
+    )}
+  </div>
 );
 
 /**
